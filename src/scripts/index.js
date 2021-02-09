ed5344bf29ddf4c3df7dd7bdf3c3ac3ca3f9fd9a
Alex <alevaliev@yandex.ru>
2021-02-09 14:21:36 +0300

    added new styles for hero adaptive sizes and menu

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -25,6 +25,14 @@ $(function() {
   wow.init();
 
   $('.hero__button').on('click', function() {
-    $(this).toggleClass('open');
+    $(this).addClass('open');
+    $('.menu').addClass('open');
+    $('.hero__link').addClass('open');
+  });
+
+  $('.menu__button').on('click', function() {
+    $('.menu').removeClass('open');
+    $('.hero__link').removeClass('open');
+    $('.hero__button').removeClass('open');
   });
 });
