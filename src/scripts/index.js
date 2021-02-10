75487eab3ac93381e23e0cc36c74eec49d32386d
Alex <alevaliev@yandex.ru>
2021-02-10 17:13:39 +0300

    added styles for secondary hero and menu added script for menu

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -25,14 +25,49 @@ $(function() {
   wow.init();
 
   $('.hero__button').on('click', function() {
-    $(this).addClass('open');
-    $('.menu').addClass('open');
-    $('.hero__link').addClass('open');
+    $(this).toggleClass('open');
+    $('.menu').toggleClass('open');
+    $('.menu__container').toggleClass('open');
+    $('.hero__link').toggleClass('open');
+
+    if ($(window).width() < 576) {
+      $('.hero__logo').toggleClass('open');
+    }
   });
 
-  $('.menu__button').on('click', function() {
-    $('.menu').removeClass('open');
-    $('.hero__link').removeClass('open');
-    $('.hero__button').removeClass('open');
+  // $('.menu__button').on('click', function() {
+  //   $('.menu').removeClass('open');
+  //   $('.menu__container').removeClass('open');
+  //   $('.hero__link').removeClass('open');
+  //   $('.hero__button').removeClass('open');
+  // });
+
+  $('a[href*="#"]')
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .on('click', function(event) {
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 900, function() {
+          var $target = $(target);
+          $target.triggerHandler( "focus" );
+          if ($target.is(":focus")) { 
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); 
+            $target.triggerHandler( "focus" );
+          };
+        });
+      }
+    }
   });
 });
